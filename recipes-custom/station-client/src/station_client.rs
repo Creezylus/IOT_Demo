9a91275ce9b5a3be3e991915c083d3b213abeaf8
creezylus <am8patil@gmail.com>
2026-08-31 12:21:42 -0700

    run locals

--- recipes-custom/station-client/src/station_client.rs
+++ recipes-custom/station-client/src/station_client.rs
@@ -5,7 +5,8 @@ use tokio::io::AsyncReadExt;
 use tokio::net::TcpListener;
 use tokio::sync::Semaphore;
 
-const HOST: &str = "192.168.1.185";
+//const HOST: &str = "192.168.1.185";
+const HOST: &str = "127.0.0.1";
 const PORT: u16 = 9090;
 const MAX_SENSORS: usize = 5;
 const MAX_EDGE: usize = 3;
---
+++ recipes-custom/test/run_local.sh
@@ -0,0 +1,4 @@
+../backend-server/target/debug/backend-server &
+../station-client/target/debug/station_client 2 50.5 10.1 &
+../edge-client/files/edge_client 1 &
+../sensor-client/files/sensor_client 1 &
