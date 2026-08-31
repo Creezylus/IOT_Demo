9da36749a1ebdfe5f06db2cd520e471e6ce8c310
creezylus <am8patil@gmail.com>
2026-08-31 12:06:46 -0700

    add setup db sh

---
+++ recipes-custom/backend-server/sql/setup_db.sh
@@ -0,0 +1,9 @@
+if [ -z "$1" ]; then
+    echo "Usage: $0 <username>"
+    exit 1
+fi
+USER="$1"
+sudo -u postgres createdb iot_metrics
+psql -U "$USER" -d iot_metrics -h localhost -f iot_metric_schema.sql
+psql -U "$USER" -d iot_metrics -h localhost -f add_staion_locations.sql
+
